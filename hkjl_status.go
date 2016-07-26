fc09c4c9fc5f382a734e46abf069e304b886ef47
Lex van Roon <r3boot@r3blog.nl>
2016-07-26 18:45:14 +0200

    YOLO

--- hkjl_status.go
+++ hkjl_status.go
@@ -2,20 +2,47 @@ package main
 
 import (
 	"flag"
-	"github.com/r3boot/hkjl_status/modules"
 	"github.com/r3boot/rlib/logger"
+	"net/http"
+	"time"
 )
+
+// URLs to monitor status for
+const MAINSITE_URL string = "https://www.hackenkunjeleren.nl/"
+const COMMUNITY_URL string = "https://community.hackenkunjeleren.nl/"
 
 const D_DEBUG bool = false
 const D_TIMESTAMP bool = false
 
-const MAINSITE_URL string = "https://www.hackenkunjeleren.nl/"
-const COMMUNITY_URL string = "https://community.hackenkunjeleren.nl/"
+type Response struct {
+	Code  int
+	Time  float64
+	Error error
+}
 
 var debug = flag.Bool("D", D_DEBUG, "Enable debugging output")
 var timestamp = flag.Bool("T", D_TIMESTAMP, "Enable timestamps in output")
 
 var Log logger.Log
+
+func PollSite(url string) (response Response) {
+	var res *http.Response
+	var err error
+	var t_start time.Time
+
+	t_start = time.Now()
+
+	res, err = http.Get(url)
+	if err != nil {
+		response.Error = err
+		return
+	}
+
+	response.Code = res.StatusCode
+	response.Time = time.Since(t_start).Seconds()
+
+	return
+}
 
 func init() {
 	flag.Parse()
@@ -24,14 +51,11 @@ func init() {
 	Log.UseVerbose = *debug
 	Log.UseTimestamp = *timestamp
 	Log.Debug("Logging initialized")
-
-	modules.Setup(Log)
-	Log.Debug("Modules initialized")
 }
 
 func main() {
 	Log.Debug("Polling and updating status information")
 
-	Log.Debug(modules.PollSite(MAINSITE_URL))
-	Log.Debug(modules.PollSite(COMMUNITY_URL))
+	Log.Debug(PollSite(MAINSITE_URL))
+	Log.Debug(PollSite(COMMUNITY_URL))
 }
